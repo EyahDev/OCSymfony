ed9859ccaab180b77f8df76f0a4b52d72e8b2911
Adrien Desmet <adrien.desmet@hotmail.fr>
2017-07-09 19:05:28 +0200

    Cours 2 du Chapitre 2

--- src/OCSymfony/Controller/AdvertController.php
+++ src/OCSymfony/Controller/AdvertController.php
@@ -7,14 +7,26 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller {
 
+    public function viewSlugAction($slug, $year, $_format) {
+
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au slug '".$slug."', créée en ".$year." et au format ".$_format."."
+        );
+    }
+
+    public function viewAction($id) {
+
+        return new Response("Affichage de l'annonce d'id : " .$id);
+    }
+
     Public function indexAction() {
-        // Récupération du contenu
-        $content = $this
-            ->get('templating')
-            ->render('OCSymfonyBundle:Advert:index.html.twig', array('nom' => 'Eyah'));
+        // Récupération du contenu avec la vue
+        $url = $this->get('router')->generate(
+            'oc_platform_view',
+            array('id' => 5)
+        );
 
-        // Génération de la vue avec le contenu
-        return new Response($content);
-
+        // Retourne la vue avec le contenu
+        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
     }
 }
