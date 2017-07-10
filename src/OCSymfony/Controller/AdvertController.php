630eeb8b69bc13f67b195de0607fdc1e2ff0f4b4
Adrien Desmet <adrien.desmet@hotmail.fr>
2017-07-10 14:34:18 +0200

    Cours 3 du Chapitre 2

--- src/OCSymfony/Controller/AdvertController.php
+++ src/OCSymfony/Controller/AdvertController.php
@@ -3,30 +3,59 @@
 namespace OCSymfony\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller {
 
-    public function viewSlugAction($slug, $year, $_format) {
+    public function deleteAction ($id) {
 
-        return new Response(
-            "On pourrait afficher l'annonce correspondant au slug '".$slug."', créée en ".$year." et au format ".$_format."."
-        );
+        return $this->render('OCSymfonyBundle:Advert:delete.html.twig');
+    }
+
+    public function editAction ($id, Request $request) {
+
+        // Vérification si la requête est en POST
+        if ($request->isMethod('POST')) {
+            // Création du message flash de confirmation
+            $request->getSession()->getFlashBag()->add('notice', 'L\'annonce à bien été modifiée');
+
+            // Redirection vers la page de visualisation de cette annonce
+            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+        }
+        // Si ce n'est pas POST
+        return $this->render('OCSymfonyBundle:Advert:edit.html.twig');
+
+    }
+
+    public function addAction(Request $request) {
+
+        // Vérification si la requête est en POST
+        if ($request->isMethod('POST')) {
+            // Création du message flash de confirmation
+            $request->getSession()->getFlashBag()->add('notice', 'L\'annonce à bien été enregistrée');
+
+            // Redirection vers la page de visualisation de cette annonce
+            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+        }
+        // Si ce n'est pas POST
+        return $this->render('OCSymfonyBundle:Advert:add.html.twig');
+
     }
 
     public function viewAction($id) {
 
-        return new Response("Affichage de l'annonce d'id : " .$id);
+        return $this->render('OCSymfonyBundle:Advert:view.html.twig', array('id' => $id));
     }
 
-    Public function indexAction() {
-        // Récupération du contenu avec la vue
-        $url = $this->get('router')->generate(
-            'oc_platform_view',
-            array('id' => 5)
-        );
+    Public function indexAction($page) {
+        // Vérification si la page est inferieur à 1
+        if ($page < 1) {
+            // Affichage d'une page d'erreur 404
+            throw new NotFoundHttpException('La page "'.$page.'" n\'existe pas.');
+        }
 
-        // Retourne la vue avec le contenu
-        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
+        // Appel du template
+        return $this->render('OCSymfonyBundle:Advert:view.html.twig');
     }
 }
